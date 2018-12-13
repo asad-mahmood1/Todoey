737db9fedf2788bfc6dad7f959ea921fa0b5dd2f
Asad Mahmood <asadmahmood@asads-macbook-pro.local>
2018-12-12 22:19:27 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Asad Mahmood on 12/12/18.
+//  Copyright © 2018 Asad Mahmood. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
